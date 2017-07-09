b6b68678997e45a1a447abf651dd26d85c22370a
allyan <allyan@walla.com>
2017-07-09 09:16:05 +0300

    Initial commit

---
+++ app/src/main/java/com/example/allyan/messaging/DefaultMessages.java
@@ -0,0 +1,9 @@
+package com.example.allyan.messaging;
+
+public class DefaultMessages
+{
+  public static String ACCEPT_CHAT_MESSAGE = "Companion accepted the invitation";
+  public static String DECLINE_CHAT_MESSAGE = "Companion declined the invitation";
+  public static String CONNECT_DEMAND = "User %s wants to chat with you";
+}
+                                            
---
+++ app/src/main/java/com/example/allyan/messaging/Defaults.java
@@ -0,0 +1,16 @@
+package com.example.allyan.messaging;
+
+public class Defaults
+{
+  public static final String APPLICATION_ID = "6B7077F2-4B55-64FC-FFAD-C526CA127A00";
+  public static final String SECRET_KEY = "A3DB7CC0-A93D-5CA2-FFDA-88CE8F460100";
+  public static final String VERSION = "v1";
+  public static final String SERVER_URL = "https://api.backendless.com";
+
+  //SET YOUR GOOGLE PROJECT ID BEFORE LAUNCH
+  //ALSO YOU SHOULD SET GOOGLE API KEY IN BACKENDLESS CONSOLE
+  public static final String GOOGLE_PROJECT_ID = "935456856295";
+
+  public static final String DEFAULT_CHANNEL = "default";
+}
+                                            
